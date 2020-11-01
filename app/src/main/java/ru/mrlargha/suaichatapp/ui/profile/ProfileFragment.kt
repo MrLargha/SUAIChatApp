76db54dd30cc1e8f96e0e18bcc7c1ef7d053fdce
MrLargha <cppcodery@gmail.com>
2020-11-02 00:35:32 +0300

    Added image picker contract

--- app/src/main/java/ru/mrlargha/suaichatapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ru/mrlargha/suaichatapp/ui/profile/ProfileFragment.kt
@@ -1,16 +1,26 @@
 package ru.mrlargha.suaichatapp.ui.profile
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import ru.mrlargha.suaichatapp.R
+import ru.mrlargha.suaichatapp.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
+
+    private val getImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri ->
+        binding.textNotifications.setImageURI(uri)
+    }
+
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,8 +28,24 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        return root
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        getImage.launch("image/*")
+
+
+//        startActivity(getPickImageIntent())
+
+        return binding.root
+    }
+
+    private fun getPickImageIntent(): Intent {
+        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePhotoIntent.putExtra("return-data", true)
+
+        return Intent.createChooser(pickIntent, "Выберите фотографию").apply {
+            putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayListOf(pickIntent, takePhotoIntent))
+        }
     }
 }
