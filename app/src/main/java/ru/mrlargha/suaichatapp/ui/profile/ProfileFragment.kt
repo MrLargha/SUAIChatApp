fec553a5ba9e11e4781bec1363fbd2ba0a46fcc6
MrLargha <cppcodery@gmail.com>
2020-11-02 09:52:09 +0300

    Added permission request

--- app/src/main/java/ru/mrlargha/suaichatapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/ru/mrlargha/suaichatapp/ui/profile/ProfileFragment.kt
@@ -1,24 +1,28 @@
 package ru.mrlargha.suaichatapp.ui.profile
 
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import ru.mrlargha.suaichatapp.R
 import ru.mrlargha.suaichatapp.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
-    private val getImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri ->
-        binding.textNotifications.setImageURI(uri)
-    }
+    private val getImageLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri ->
+            binding.textNotifications.setImageURI(uri)
+        }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            getImageLauncher.launch("image/*")
+        }
 
     private lateinit var binding: FragmentProfileBinding
 
@@ -27,25 +31,16 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        getImage.launch("image/*")
-
-
-//        startActivity(getPickImageIntent())
-
-        return binding.root
-    }
-
-    private fun getPickImageIntent(): Intent {
-        val pickIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
-        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        takePhotoIntent.putExtra("return-data", true)
-
-        return Intent.createChooser(pickIntent, "Выберите фотографию").apply {
-            putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayListOf(pickIntent, takePhotoIntent))
+        binding.button.setOnClickListener {
+            if (requireContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            } else {
+                getImageLauncher.launch("image/*")
+            }
         }
+        return binding.root
     }
 }
